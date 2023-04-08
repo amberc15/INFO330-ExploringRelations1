9a18166d204ac8b597b64dc61de08828f1ad1fba
amberc15 <ambercarbajal33@gmail.com>
2023-04-08 13:19:07 -0700

    SQL

---
+++ question-2.sql
@@ -0,0 +1,2 @@
+--What are the full name(s) (last and first, in sorted order by last name) who have an age less than 30 (meaning age 29 and down)?
+SELECT last_name, first_name FROM persons WHERE age > 30;
